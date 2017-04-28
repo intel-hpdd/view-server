26544a93822b5c7c78cc6e3c7aff813187b13258
Joe Grund <grundjoseph@gmail.com>
2017-04-28 06:19:59 -0400

    bump req
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- flow-typed/npm/jest_v19.x.x.js
+++ flow-typed/npm/jest_v19.x.x.js
@@ -17,7 +17,7 @@ type JestMockFn = {
      * An array that contains all the object instances that have been
      * instantiated from this mock function.
      */
-    instances: mixed,
+    instances: mixed
   },
   /**
    * Resets all information stored in the mockFn.mock.calls and
@@ -54,15 +54,15 @@ type JestMockFn = {
   /**
    * Sugar for only returning a value once inside your mock
    */
-  mockReturnValueOnce(value: any): JestMockFn,
-}
+  mockReturnValueOnce(value: any): JestMockFn
+};
 
 type JestAsymmetricEqualityType = {
   /**
    * A custom Jasmine equality tester
    */
-  asymmetricMatch(value: mixed): boolean,
-}
+  asymmetricMatch(value: mixed): boolean
+};
 
 type JestCallsType = {
   allArgs(): mixed,
@@ -71,20 +71,20 @@ type JestCallsType = {
   count(): number,
   first(): mixed,
   mostRecent(): mixed,
-  reset(): void,
-}
+  reset(): void
+};
 
 type JestClockType = {
   install(): void,
   mockDate(date: Date): void,
   tick(): void,
-  uninstall(): void,
-}
+  uninstall(): void
+};
 
 type JestMatcherResult = {
-  message?: string | ()=>string,
-  pass: boolean,
-}
+  message?: string | (() => string),
+  pass: boolean
+};
 
 type JestMatcher = (actual: any, expected: any) => JestMatcherResult;
 
@@ -224,8 +224,8 @@ type JestExpectType = {
    * Use .toThrowErrorMatchingSnapshot to test that a function throws a error
    * matching the most recent snapshot when it is called.
    */
-  toThrowErrorMatchingSnapshot(): void,
-}
+  toThrowErrorMatchingSnapshot(): void
+};
 
 type JestObjectType = {
   /**
@@ -351,12 +351,12 @@ type JestObjectType = {
    * Creates a mock function similar to jest.fn but also tracks calls to
    * object[methodName].
    */
-  spyOn(object: Object, methodName: string): JestMockFn,
-}
+  spyOn(object: Object, methodName: string): JestMockFn
+};
 
 type JestSpyType = {
-  calls: JestCallsType,
-}
+  calls: JestCallsType
+};
 
 /** Runs this function after every test inside this context */
 declare function afterEach(fn: Function): void;
@@ -398,7 +398,7 @@ declare var it: {
    * @param {string} Name of Test
    * @param {Function} Test
    */
-  concurrent(name: string, fn?: Function): ?Promise<void>,
+  concurrent(name: string, fn?: Function): ?Promise<void>
 };
 declare function fit(name: string, fn: Function): ?Promise<void>;
 /** An individual test unit */
@@ -417,7 +417,7 @@ declare var expect: {
   /** The object that you want to make assertions against */
   (value: any): JestExpectType,
   /** Add additional Jasmine matchers to Jest's roster */
-  extend(matchers: {[name:string]: JestMatcher}): void,
+  extend(matchers: { [name: string]: JestMatcher }): void,
   /** Add a module that formats application-specific data structures. */
   addSnapshotSerializer(serializer: (input: Object) => string): void,
   assertions(expectedAssertions: number): void,
@@ -427,7 +427,7 @@ declare var expect: {
   objectContaining(value: Object): void,
   /** Matches any received string that contains the exact expected string. */
   stringContaining(value: string): void,
-  stringMatching(value: string | RegExp): void,
+  stringMatching(value: string | RegExp): void
 };
 
 // TODO handle return type
@@ -435,7 +435,7 @@ declare var expect: {
 declare function spyOn(value: mixed, method: string): Object;
 
 /** Holds all functions related to manipulating test runner */
-declare var jest: JestObjectType
+declare var jest: JestObjectType;
 
 /**
  * The global Jamine object, this is generally not exposed as the public API,
@@ -448,7 +448,9 @@ declare var jasmine: {
   arrayContaining(value: Array<mixed>): void,
   clock(): JestClockType,
   createSpy(name: string): JestSpyType,
-  createSpyObj(baseName: string, methodNames: Array<string>): {[methodName: string]: JestSpyType},
+  createSpyObj(baseName: string, methodNames: Array<string>): {
+    [methodName: string]: JestSpyType
+  },
   objectContaining(value: Object): void,
-  stringMatching(value: string): void,
-}
+  stringMatching(value: string): void
+};
