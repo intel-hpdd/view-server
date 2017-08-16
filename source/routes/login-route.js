c5e3233dbd13fc69aea4dfff255aad00c6649c9f
Will Johnson <william.c1.johnson@gmail.com>
2017-08-16 12:20:44 -0400

    Remove eula from view server (#21)
    Now that the eula is no longer required we can remove any references to
    it in the view server.
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- source/routes/login-route.js
+++ source/routes/login-route.js
@@ -7,54 +7,20 @@
 
 import viewRouter from '../view-router.js';
 import templates from '../lib/templates.js';
-import apiRequest from '../lib/api-request.js';
-import renderRequestError from '../lib/render-request-error.js';
 
 const indexTemplate = templates['index.html'];
 
 export default () => {
   viewRouter
     .route('/ui/login')
-    /**
-   * If the user is already authenticated and the eula is accepted, redirects to index page.
-   * If the user is already authenticated and the eula is not accepted, logs the user out.
-   * @param {Object} req
-   * @param {Object} res
-   * @param {Object} data
-   * @param {Function} next
-   */
-    .get(function checkEula(req, res, data, next) {
+    .get((req, res, data, next) => {
       const session = data.cache.session;
 
-      if (!session.user) return goToNext();
+      if (session.user) return res.redirect('/ui/');
 
-      if (session.user.eula_state === 'pass') return res.redirect('/ui/');
-      else
-        apiRequest({
-          path: '/session',
-          method: 'delete',
-          headers: { cookie: data.cacheCookie }
-        })
-          .stopOnError(
-            renderRequestError(
-              res,
-              err => 'Exception rendering resources: ' + err.stack
-            )
-          )
-          .each(goToNext);
-
-      function goToNext() {
-        next(req, res, data.cache);
-      }
+      next(req, res, data.cache);
     })
-    /**
-   * Renders the login page.
-   * @param {Object} req
-   * @param {Object} res
-   * @param {Object} cache
-   * @param {Function} next
-   */
-    .get(function renderLogin(req, res, cache, next) {
+    .get((req, res, cache, next) => {
       res.clientRes.setHeader('Content-Type', 'text/html; charset=utf-8');
       res.clientRes.statusCode = 200;
 
